95ad02252255e9e04a68485dde1c05500ffca32a
Jonathan Mai <h.jmai2791@gmail.com>
2018-09-17 19:04:57 -0700

    Routing started

--- src/index.js
+++ src/index.js
@@ -3,10 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import Eventlist from './components/eventList/eventList'
 
 ReactDOM.render(<App />, document.getElementById('root'));
-// ReactDOM.render(<Eventlist />, document.getElementById('root'));
-
 
 registerServiceWorker();
