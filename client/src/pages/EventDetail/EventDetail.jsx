0ddc078971d7fd4cbdeaa4f7ab664159c7b604a2
hvoncannon <hayden.voncannon@gmail.com>
2018-09-22 10:29:34 -0700

    cleaned up merged event detail code

--- client/src/pages/EventDetail/EventDetail.jsx
+++ client/src/pages/EventDetail/EventDetail.jsx
@@ -1,8 +1,12 @@
 import React, { Component } from 'react';
 import axios from "axios";
+export default EventDetail;
+import EventDescription from '../../components/EventDetail/EventDescription';
+import GoogleApiWrapper from '../../components/Map/EventMap/EventMap';
+import "../../components/EventDetail/Eventpage/eventpagestyle.css";
 
-class EventDetail extends Component {
-
+ class EventDetail extends Component {
+     
     componentDidMount() {
         const id = this.props.match.params.id;
         axios.get("/api/eventdetail/" + id)
@@ -13,39 +17,7 @@ class EventDetail extends Component {
                 })
             });
     };
-    // state = {  }
-    render() {
-        return (
-            <div className='container'>
-                <h1>Event Name</h1>
-                <button>Join</button>
-                <img alt='map' src='https://via.placeholder.com/200x200' />
 
-                <h2>Event Description</h2>
-
-                <span>Address:</span>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ac tortor neque. Sed quam dui, vulputate vitae scelerisque sit amet, venenatis vel purus. Aliquam aliquam pharetra efficitur. Suspendisse scelerisque malesuada dolor, et lobortis neque blandit id. Sed sit amet diam at nibh feugiat tempus nec quis erat. Ut laoreet euismod ante sed molestie. Nulla euismod blandit sapien, eget suscipit tortor fringilla in. Maecenas fermentum fringilla enim, ut volutpat nisl rhoncus quis.</p>
-
-                <div>
-                    <span>People Attending</span>
-                </div>
-
-                <div>
-                    <span>Comments</span>
-                </div>
-
-            </div>
-        );
-    }
-}
-
-export default EventDetail;
-import EventDescription from '../../components/EventDetail/EventDescription';
-import GoogleApiWrapper from '../../components/Map/EventMap/EventMap';
-import "../../components/EventDetail/Eventpage/eventpagestyle.css";
-
-
- class EventDetail extends Component {
   render() {
     return (
       
