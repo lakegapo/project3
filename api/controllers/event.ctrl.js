2cbdef100b2bb542f01131a888a72e2b187f8add
Jonathan Mai <h.jmai2791@gmail.com>
2018-09-26 19:15:41 -0700

    setup route

--- api/controllers/event.ctrl.js
+++ api/controllers/event.ctrl.js
@@ -10,8 +10,8 @@ module.exports = {
       citystate: req.body.citystate,
       zip: req.body.zip,
       description: req.body.description,
-      UserId : req.body.UserId
-      // category: req.body.category
+      UserId : req.body.UserId,
+      category: req.body.category
     };
     models.Event.create(event)
       .then(resp => {
