482a3705dc5aaeb757442847094610fdbabf1753
Jason Hong <JaySunHong@Jasons-MacBook-Pro.local>
2018-10-01 12:56:02 -0700

    update

--- client/src/components/Events/EventsListItems.jsx
+++ client/src/components/Events/EventsListItems.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { Container, Row, Col } from "../Grid";
+import "./EventsListItems.css";
 
 export const EventsListItems = props => (
-  <li className="list-group-item">
+  <li id="eventslistitems" className="list-group-item">
     <Container>
-      <Row>
+      <Row fluid>
         <Col size="sm-12">
           <a href={`/eventdetail/${props.href}`}><h3>Title: {props.title}</h3></a>
           <p>Summary: {props.description}</p>
