39a240706c4aa69310863c07009b4ddf4b7a6453
Jason Hong <JaySunHong@dhcp-v108-113.mobile.uci.edu>
2018-10-01 19:34:02 -0700

    changed category button to create event button.

--- client/src/components/Events/EventsList.jsx
+++ client/src/components/Events/EventsList.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import "./EventsList.css";
 
 export const EventsList = props => (
+  
   <div>
     <h3 id="eventsListTitle">Events</h3>
     <ul id="accordionEvents" className="accordion list-group">{props.children}</ul>
