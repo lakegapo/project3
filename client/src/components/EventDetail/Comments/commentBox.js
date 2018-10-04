60a39ef1ec98c7875baf6588d3b805178b695c56
Marvie Solis <marvie_solis@ymail.com>
2018-10-03 19:02:06 -0700

    Fixed the comment button and text area from overflowing from page

--- client/src/components/EventDetail/Comments/commentBox.js
+++ client/src/components/EventDetail/Comments/commentBox.js
@@ -2,10 +2,18 @@ import React from "react";
 import "./commentBox.css";
 
 const CommentBox = props => (
-  <div>
-  <textarea onChange={props.handleChange} name="commentBody" id="editAddress" cols="47" rows="3" placeholder="Comment here..."></textarea>
-  <button className="btn commentButton" onClick={props.handleClick}>Comment</button>
-</div>
+  <div className="commentBoxDiv">
+    <div className="row justify-content-center">
+      <div className="col-sm-auto">
+        <textarea onChange={props.handleChange} name="commentBody" id="editAddress" cols="47" rows="3" placeholder="Comment here..."></textarea>
+      </div>
+    </div>
+    <div className="row justify-content-center">
+      <div className="col-sm-auto">
+        <button className="btn commentButton" onClick={props.handleClick}>Comment</button>
+      </div>
+    </div>
+  </div>
 );
 
 export default CommentBox;
